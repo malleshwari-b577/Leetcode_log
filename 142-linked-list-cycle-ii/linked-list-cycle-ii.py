33ea2f68be8cf51554c3531ea821174074c8b86b
Malleshwari Bhimagoni <bmalleshwari2111@gmail.com>
2026-07-16 12:46:01 +0530

    Time: 51 ms (69.13%) | Memory: 22.3 MB (75.67%) - LeetSync

--- 142-linked-list-cycle-ii/linked-list-cycle-ii.py
+++ 142-linked-list-cycle-ii/linked-list-cycle-ii.py
@@ -6,20 +6,18 @@
 
 class Solution:
     def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        fp=sp=head
-        while fp is not None and fp.next is not None:
-            fp=fp.next.next
-            sp=sp.next
+        fp=sp=head #keep two pointers 
+        while fp and fp.next:
+            fp=fp.next.next #fast pointer 2 steps
+            sp=sp.next      #slow pointer 1 step
 
+            #if fp and sp meets there is cycle
             if fp==sp:
-                break
-
-        else:
-            return None
-
-        fp=head 
-        while fp is not sp:
-            fp=fp.next
-            sp=sp.next
-        
-        return fp
+                #reset one pointer to head other same node of intersection
+                fp=head
+                #when these two pointers meet then it is head 
+                while fp is not sp:
+                    fp=fp.next      #move each pointer 1 step
+                    sp=sp.next
+                return fp
+        return None
