9c377b92f45852cdc32c49be0bd2d8559d0dbcf2
Malleshwari Bhimagoni <bmalleshwari2111@gmail.com>
2026-09-23 18:57:48 +0530

    Time: 4 ms (43.07%) | Memory: 23.4 MB (76.99%) - LeetSync

--- 890-lemonade-change/lemonade-change.py
+++ 890-lemonade-change/lemonade-change.py
@@ -3,20 +3,34 @@ class Solution:
         five=0 ; ten =0
 
         for i in bills:
-            if i==5:
+            #iteration of all
+
+            #if it is 5
+            if i == 5:
+                #add one to five 
                 five+=1
-            elif i==10:
+            
+            #if it is 10
+            elif i == 10 :
+                #add one to 10 ; remove form 5
                 if five:
-                    five-=1 
+                    five-=1
                     ten+=1
+                #if no change of 5 rs
                 else:
                     return False
-            elif i==20:
+            
+            #if it is 20
+            else:
+                #check if we have 5 and 10
                 if five and ten:
                     five-=1 
                     ten-=1
+                #if it has therr or more 5s
                 elif five>=3:
                     five-=3
+                #if not then return false
                 else:
                     return False
+        
         return True
